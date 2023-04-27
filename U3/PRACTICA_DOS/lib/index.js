eb886d79aefa1c89c996fc592bce77dd3f2b6331
AdrianG <guevadev@gmail.com>
2023-04-26 23:52:52 -0600

    Actividad botones

---
+++ U3/PRACTICA_DOS/lib/index.js
@@ -0,0 +1,12 @@
+document.getElementById("btnRojo").addEventListener(
+    "click",()=>{document.body.style.backgroundColor="red"});
+
+document.getElementById("btnVerde").addEventListener(
+    "click",()=>{document.body.style.backgroundColor="green"});
+
+document.getElementById("btnAzul").addEventListener(
+    "click",()=>{document.body.style.backgroundColor="blue"});
+
+document.getElementById("btnClean").addEventListener(
+    "click",()=>{document.body.style.backgroundColor="white"});
+
